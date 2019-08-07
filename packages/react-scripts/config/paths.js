e0bbca8ec74bff66cd73d612a7aa5158d4c3bdcd
Jiří Čermák <jiri.cermak@ackee.cz>
2019-08-07 21:26:26 +0200

    Inject aliases dynamically to jsconfig

--- packages/react-scripts/config/paths.js
+++ packages/react-scripts/config/paths.js
@@ -77,6 +77,7 @@ module.exports = {
   prettierConfig: resolveApp('prettier.config.js'),
   appConfig: resolveApp('config/custom'),
   webpackConfig: resolveApp('config/webpack.config.js'),
+  appAliases: resolveApp('config/aliases.js'),
   // @ackee/react-scripts - end
 };
 
@@ -103,6 +104,7 @@ module.exports = {
   // @ackee/react-scripts - beginning
   prettierConfig: resolveApp('prettier.config.js'),
   appConfig: resolveApp('config'),
+  appAliases: resolveApp('config/aliases.js'),
   // @ackee/react-scripts - end
 
   // These properties only exist before ejecting:
@@ -147,6 +149,7 @@ if (
     // @ackee/react-scripts - beginning
     prettierConfig: resolveOwn('template/prettier.config.js'),
     appConfig: resolveOwn('template/config'),
+    appAliases: resolveOwn('config/aliases.js'),
     // @ackee/react-scripts - end
 
     // These properties only exist before ejecting:
