7001a4d7e31ee3b158489c1c58b56c9c8834da7c
Jiří Čermák <jiri.cermak@ackee.cz>
2019-08-12 18:25:16 +0200

    init/useAuthModule: fix path to dependency files

--- packages/react-scripts/config/paths.js
+++ packages/react-scripts/config/paths.js
@@ -119,6 +119,7 @@ module.exports = {
   // @ackee/react-scripts - beginning
   webpackConfig: resolveOwn('config/webpack.config.js'),
   prettierConfig: resolveOwn('template/prettier.config.js'),
+  templateSrc: resolveOwn('template/src'),
   // @ackee/react-scripts - end
 };
 
