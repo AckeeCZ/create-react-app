462660ef6cf78d49271e241d450886a1f76285ea
Jiří Čermák <jiri.cermak@ackee.cz>
2019-11-12 11:05:37 +0100

    Move custom paths from react-scripts paths to the custom dir

--- packages/react-scripts/config/paths.js
+++ packages/react-scripts/config/paths.js
@@ -114,10 +114,6 @@ module.exports = {
   ownNodeModules: resolveOwn('node_modules'), // This is empty on npm 3
   appTypeDeclarations: resolveApp('src/react-app-env.d.ts'),
   ownTypeDeclarations: resolveOwn('lib/react-app.d.ts'),
-
-  // @ackee/react-scripts - beginning
-  webpackConfig: resolveOwn('config/webpack.config.js'),
-  // @ackee/react-scripts - end
 };
 
 const ownPackageJson = require('../package.json');
@@ -159,10 +155,6 @@ if (
     ownNodeModules: resolveOwn('node_modules'),
     appTypeDeclarations: resolveOwn(`${templatePath}/src/react-app-env.d.ts`),
     ownTypeDeclarations: resolveOwn('lib/react-app.d.ts'),
-
-    // @ackee/react-scripts - beginning
-    webpackConfig: resolveOwn('config/webpack.config.js'),
-    // @ackee/react-scripts - end
   };
 }
 // @remove-on-eject-end
