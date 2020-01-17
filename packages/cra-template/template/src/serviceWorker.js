3a8d02eb56b5ec71f9f9103b8f7acfb812be5ea9
Jiří Čermák <jiri.cermak@ackee.cz>
2020-01-17 11:51:15 +0100

    cra-template-typescript: add basic configs

--- packages/cra-template/template/src/serviceWorker.js
+++ packages/cra-template/template/src/serviceWorker.js
@@ -15,7 +15,7 @@ const isLocalhost = Boolean(
         // [::1] is the IPv6 localhost address.
         window.location.hostname === '[::1]' ||
         // 127.0.0.0/8 are considered localhost for IPv4.
-        window.location.hostname.match(/^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/)
+        window.location.hostname.match(/^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/),
 );
 
 export function register(config) {
@@ -41,7 +41,7 @@ export function register(config) {
                 navigator.serviceWorker.ready.then(() => {
                     console.log(
                         'This web app is being served cache-first by a service ' +
-                            'worker. To learn more, visit https://bit.ly/CRA-PWA'
+                            'worker. To learn more, visit https://bit.ly/CRA-PWA',
                     );
                 });
             } else {
@@ -69,7 +69,7 @@ function registerValidSW(swUrl, config) {
                             // content until all client tabs are closed.
                             console.log(
                                 'New content is available and will be used when all ' +
-                                    'tabs for this page are closed. See https://bit.ly/CRA-PWA.'
+                                    'tabs for this page are closed. See https://bit.ly/CRA-PWA.',
                             );
 
                             // Execute callback
@@ -97,24 +97,6 @@ function registerValidSW(swUrl, config) {
 }
 
 function checkValidServiceWorker(swUrl, config) {
-<<<<<<< HEAD
-  // Check if the service worker can be found. If it can't reload the page.
-  fetch(swUrl, {
-    headers: { 'Service-Worker': 'script' },
-  })
-    .then(response => {
-      // Ensure service worker exists, and that we really are getting a JS file.
-      const contentType = response.headers.get('content-type');
-      if (
-        response.status === 404 ||
-        (contentType != null && contentType.indexOf('javascript') === -1)
-      ) {
-        // No service worker found. Probably a different app. Reload the page.
-        navigator.serviceWorker.ready.then(registration => {
-          registration.unregister().then(() => {
-            window.location.reload();
-          });
-=======
     // Check if the service worker can be found. If it can't reload the page.
     fetch(swUrl, {
         headers: { 'Service-Worker': 'script' },
@@ -136,16 +118,17 @@ function checkValidServiceWorker(swUrl, config) {
         })
         .catch(() => {
             console.log('No internet connection found. App is running in offline mode.');
->>>>>>> cra-template: fix syntax error in serviceWorker.js
         });
 }
 
 export function unregister() {
     if ('serviceWorker' in navigator) {
-        navigator.serviceWorker.ready.then(registration => {
-            registration.unregister();
-        }).catch(error => {
-          Log.error(error.message);
-        });;
+        navigator.serviceWorker.ready
+            .then(registration => {
+                registration.unregister();
+            })
+            .catch(error => {
+                Log.error(error.message);
+            });
     }
 }
